19baffc29d122c3825afe0b3e9f7c9bee4f8f2f5
diCugno <dialessandro@itis-molinari.eu>
2025-02-11 21:37:21 +0100

    aggiunta tabella e modifiche

--- scripts/components.js
+++ scripts/components.js
@@ -64,4 +64,179 @@ export const createNavigator = (parentElement) => {
     }
     window.addEventListener('popstate', render); 
     render();   
- }
+}
+
+export function createTable(parentElement, pubsub) {
+    let dati;
+
+    return {
+
+        setData: (newData) => {
+            dati = newData;
+        },
+
+        renderTableAdmin: () => {
+          let html = '';
+
+          // Struttura della tabella
+          html += '<table class="table table-bordered table-striped table-container">';
+          html += `
+          <thead>
+              <tr>
+                  <th>Nome</th>
+                  <th>Foto</th>
+                  <th>Azioni</th>
+              </tr>
+          </thead>
+          <tbody>
+          `;            
+
+          // Aggiunta delle righe con i dati filtrati
+          dati.forEach((e) => {
+              html += `
+              <tr>
+                  <td>${e.nome}</td>
+                  <td>
+                      <img src="${e.foto}" alt="${e.nome}" style="width: 150px; height: auto;">
+                  </td>
+                  <td>
+                    <button type="button" id="rimuovi" class="btn btn-secondary">Rimuovi</button>
+                  </td>
+              </tr>
+              `;
+          });
+          
+          html += '</tbody></table>';
+          parentElement.innerHTML = html;
+          const rimuovi=document.getElementById("rimuovi");
+          rimuovi.onclick = () => {
+                console.log("RIMUOVII");
+          };     
+        },
+
+
+    };
+}
+
+export function createLogin(parentElement, myToken, pubsub) {
+    let isLogged = false;
+
+    return {
+        createModal: (login_btn) => {
+            console.log("isLogged     ",isLogged)
+
+            const loginContainer = parentElement;
+
+            const loginHTML = `
+        
+            <div id="loginModal" class="modal" tabindex="-1" style="display: none;">
+            <div class="modal-dialog">
+                <div class="modal-content">
+        
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5">Login</h1>
+                </div>
+        
+                <div class="modal-body">
+                    <form id="loginForm">
+        
+                    <div class="form-group">
+                        <label for="user">User</label>
+                        <input type="text" class="form-control" id="user" required>
+                    </div>
+        
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" required>
+                    </div>
+        
+                    <button type="button" id="submitBtn" class="btn btn-primary">Invia</button>
+                    <button type="button" id="cancelBtn" class="btn btn-secondary">Annulla</button>
+                    </form>
+                </div>
+                </div>
+            </div>
+            </div>
+            `;
+        
+            loginContainer.innerHTML += loginHTML;
+        
+            const modal = document.getElementById('loginModal');
+            const cancelBtn = document.getElementById('cancelBtn');
+            const submitBtn = document.getElementById('submitBtn');
+        
+  
+  
+        const login = (username, password) => {
+        return new Promise((resolve, reject) => {
+            fetch("http://ws.cipiaceinfo.it/credential/login", {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                    "key": myToken
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password
+                })
+            })
+            .then(r => r.json())
+            .then(r => {
+                resolve(r.result);
+            })
+            .catch(reject);
+        });
+        };
+  
+  
+        login_btn.onclick = () => {
+        modal.style.display = 'block';
+        };
+    
+        cancelBtn.onclick = () => {
+        modal.style.display = 'none';
+        };
+  
+
+        submitBtn.onclick = () => {
+        const inputName = document.getElementById('user').value;
+        const inputPassword = document.getElementById('password').value;
+        login(inputName, inputPassword).then((result) => {
+            console.log("RESULTTTTT     ",result);
+            if (result) {
+                isLogged = true;
+                pubsub.publish("Logged",isLogged);
+                console.log("login riuscito");
+                console.log(inputName);
+                console.log(inputPassword);
+                modal.style.display = 'none';
+            } else {
+                console.log("login fallita");
+            }
+        });
+        };
+        }
+    }
+  
+}
+
+export const createPubSub = () => {
+const dict = {};
+return {
+    subscribe: (eventName, callback) => {
+        // controllo se esiste l'evento, senno lo creo
+        if (!dict[eventName]) {
+            dict[eventName] = [];
+        }
+        // aggiungo la callback
+        dict[eventName].push(callback);
+    },
+    publish: (eventName, data) => {
+        // controllo se ce gia una callback per l'evento
+        if (dict[eventName]) {
+            // per ogni callback, invio anche i dati
+            dict[eventName].forEach((callback) => callback(data));
+        }
+    }
+}
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,26 @@
-import { createMiddleware, controller, createNavigator } from './components.js';
+import { createMiddleware, controller, createNavigator, createLogin, createPubSub, createTable } from './components.js';
+
+const login_btn = document.getElementById('login');
+const tableAdmin = document.getElementById('table-container-admin');
+
+let myToken, myKey;
+
+let foto = [
+        {
+        "nome" : "jerome",
+        "foto" : "https://i.postimg.cc/mktDtpgj/JEROME.jpg"
+        }
+        ]
 
 const nav = createNavigator(document.querySelector('#container'));
+const pubsub = createPubSub();
+
+let table = createTable(tableAdmin,pubsub);
+
+
+function render(){
+    console.log("RENDERRR")
+  table.setData(foto);
+  table.renderTableAdmin();
+}
+render();
