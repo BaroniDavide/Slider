6ff5d165f77705a78c9042ff7b0b7426b5745fa4
Davide Baroni <baronidavide@itis-molinari.eu>
2025-02-11 12:17:20 +0100

    aggiunta delete nel js

--- index.js
+++ index.js
@@ -6,16 +6,46 @@ const createMiddleware= () =>{
             return json;
         },
         delete: async(id) =>{
-            const response= await fetch("")
+            const response= await fetch("/delete/"+ id, {
+                method: 'DELETE',
+            });
+            const json= await response.json();
+            return json
+
         },
         upload: async(inputFile) =>{
-            FormData.append("file", inputFile.files[0]);
-            const body= FormData;
+            formData= new FormData();
+            formData.append("file", inputFile.files[0]);
+            const body= formData;
             const fetchOptions={
                 method: 'post',
                 body: body
             };
+            try{
+                const res= await fetch("/upload", fetchOptions);
+                const data= await res.json();
+                console.log(data);
+            }catch (e){
+                console.log(e);
+            }
         
         }
     }
 }
+
+const controller= async(middleware)=>{
+    const template= `<li> `
+
+    const inputFile= document.querySelectorAll('#file');
+    const button= document.querySelector('#button');
+    const listUL= document.getElementById('listUL');
+
+    handleSubmit= async(event)=>{
+        await middleware.upload(inputFile);
+        const list= await middleware.load();
+        render(list);
+    }
+    button.onclick= handleSubmit;
+    mi
+}
+
