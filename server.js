50cc4d1be13a1c1fd4329610985d05776eb5f74f
Davide Baroni <baronidavide@itis-molinari.eu>
2025-02-11 11:24:15 +0100

    server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const express= require("express");
+const http= require('http');
+const app= express();
+const multer= require('multer');
+const database= require("./database");
+const storage= multer.diskStorage({
+    filename: function(req,file,callback){
+        callback(null,file.originalname);
+    }
+});
+
+database.createTable();
+const upload= multer({storage: storage}).single('file');
+app.use("/",express.static(path.join(__dirname, "public")));
+app.use("/files", express.static(path.join(__dirname,"files")));
+app.post("/upload", multer({storage: storage}).single('file'), async(req,res)=> {
+    await database.insert("./files/" + req.file.originalname);
+    res.json({result: "ok"});
+});
+
+app.delete('/delete/:id', async(req,res) =>{
+    await database.delete(req.params.id);
+    res.json({result:"ok"});
+  })
+
+  const server= http.createServer(app);
+server.listen(5600, () =>{
+    console.log("- server running");
+})
